8d148fb2f6b71d7ac0f00945fa45b82ca466ee39
YOGATHASAN PIRUNTHAPAN <71667604+PRINCEREX1997@users.noreply.github.com>
2022-09-08 09:43:20 +0530

    First Release

---
+++ QRReaderApplication/android/app/src/main/kotlin/com/example/qrreaderapplication/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qrreaderapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ QRReaderApplication/lib/main.dart
@@ -0,0 +1,121 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+import 'firebase_options.dart';
+
+var db = FirebaseFirestore.instance;
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text(
+                      'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                  : Text('Scan QR code'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+        var x = result!.code;
+        List<String> userInfo = x.toString().split("-");
+        AddUser(userInfo[0], userInfo[1], userInfo[2], userInfo[3], userInfo[4],
+            userInfo[5], userInfo[6], userInfo[7]);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void AddUser(var id, var fName, var lName, var age, var hNumber, var sName,
+      var city, var country) {
+    final UserData = {
+      "ID": id,
+      "FirstName": fName,
+      "LastName": lName,
+      "Age": age,
+      "HouseNumber": hNumber.toString(),
+      "StreetName": sName.toString(),
+      "City": city,
+      "Country": country
+    };
+    db.collection("UserDetails").doc(id).set(UserData);
+  }
+}
